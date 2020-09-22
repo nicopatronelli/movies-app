4a39dfec4df573cd125d16370c569e6f3749e5f6
nicopatronelli <nicopatronelli@gmail.com>
2020-09-22 17:26:13 -0300

    front-end & back-end initial

---
+++ back-end/src/main/java/com/example/moviesappbackend/MoviesAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviesappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAppBackEndApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/example/moviesappbackend/MoviesAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
